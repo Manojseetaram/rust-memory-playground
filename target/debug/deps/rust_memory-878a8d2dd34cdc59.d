17aef72c953d379b5b19bcc3fbbccee3018d4b33
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-10-20 18:20:50 +0530

    ignore the target file

--- target/debug/deps/rust_memory-878a8d2dd34cdc59.d
+++
@@ -1,5 +0,0 @@
-/Users/manojseetaramgowda/projects/rust-memory/target/debug/deps/rust_memory-878a8d2dd34cdc59.d: src/main.rs
-
-/Users/manojseetaramgowda/projects/rust-memory/target/debug/deps/librust_memory-878a8d2dd34cdc59.rmeta: src/main.rs
-
-src/main.rs:
