f4748fb8af312c6015ec6ec5573f17af347211cb
Manojseetaram <manojseetaram.artytech@gmail.com>
2025-10-20 19:15:51 +0530

    self-reference example

--- src/reference.rs
+++ src/reference.rs
@@ -1,3 +1,35 @@
+use std::pin::Pin;
+struct S {
+    text: String,
+    slice: *const str,
+}
+
+impl S {
+    fn new(data: String) -> Pin<Box<S>> {
+        //Step 1 : temporary dummy self-reference
+        let mut boxed = Box::pin(S {
+            text: data,
+            slice: "".as_ref() as *const str,
+        });
+        //Step 2 : set correct reference after pinning
+        let data_ref: *const str = boxed.text.as_str();
+        unsafe {
+            let mut_ref = Pin::as_mut(&mut boxed);
+            let this = Pin::get_unchecked_mut(mut_ref);
+            this.slice = data_ref;
+        }
+        boxed
+    }
+    fn show(self: Pin<&Self>) {
+        println!("Data : {}", self.text);
+        unsafe {
+            println!("Reference : {}", &*self.slice);
+        }
+    }
+}
+
 pub fn reference_operation() {
     println!("Reference operation running...in rust code");
+    let s = S::new(String::from("Hello"));
+    s.as_ref().show();
 }
